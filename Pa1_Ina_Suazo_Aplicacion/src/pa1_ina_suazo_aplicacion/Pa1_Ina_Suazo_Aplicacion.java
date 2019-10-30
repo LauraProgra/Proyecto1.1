fb63694e5085b99f49355a25d1418ad6d0a49ad3
Unknown <suazo7926@gmail.com>
2019-10-29 18:29:40 -0600

    proyecto1

---
+++ Pa1_Ina_Suazo_Aplicacion/src/pa1_ina_suazo_aplicacion/Pa1_Ina_Suazo_Aplicacion.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pa1_ina_suazo_aplicacion;
+
+/**
+ *
+ * @author inalaurasuazo
+ */
+public class Pa1_Ina_Suazo_Aplicacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
